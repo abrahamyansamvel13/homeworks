fbc184a1eafb7cf9f646cfefe5034fc23451c5a1
abrahamyansamvel13 <abrahamyansamvel1@gmail.com>
2026-02-22 19:48:34 +0400

    find with path

--- homework1/findMaxSizeFile.sh
+++ homework1/findMaxSizeFile.sh
@@ -1,10 +1,15 @@
 #!/bin/bash 
 
-find . -type f -exec du -h {} + | sort -hr | head -n 1
+#find . -type f -exec du -h {} + | sort -hr | head -n 1
 
 #find . -type  to finde all files
 #du -h for size
 # sort -hr sort from max to min
 #head -n 1  display max size file
 
+DIR="${1:-.}"
 
+find "$DIR" \
+  -type d \( -path /proc -o -path /sys -o -path /dev \) -prune -o \
+  -type f -exec du -h {} + \
+| sort -hr | head -n 1
